04ee13ff56c911bf21f9b81773f300e2b5f4c7ea
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-06 14:25:38 -0400

    Add basic API routes for displaying pinned sentences

---
+++ app/controllers/pinned_results_controller.rb
@@ -0,0 +1,10 @@
+class PinnedResultsController < ApplicationController
+
+  def index
+    @data = 'data'
+    respond_to do |format|
+      format.html { render 'index' }
+      format.json { render json: @data, status: 200 }
+    end
+  end
+end
