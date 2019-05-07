d58f3c91e4391b001f878f7afe4b702932fc2b25
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-07 09:55:23 -0400

    Remove specific ruby version from gemfile. Add mock API fetch results to be rendered by json in controller

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.5.1'
+# Remove ruby version
+# ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1', '>= 5.2.1.1'
--- app/controllers/pinned_sentences_controller.rb
+++ app/controllers/pinned_sentences_controller.rb
@@ -1,10 +1,47 @@
-class PinnedSentencesController < ApplicationController
+class API::V1::PinnedSentencesController < ApplicationController
+
+  @results = [
+      {
+        "id": "example",
+        "language": "en",
+        "lexicalEntries": [
+          {
+            "inflectionOf": [
+              {
+                "id": "example",
+                "text": "example"
+              }
+            ],
+            "language": "en",
+            "lexicalCategory": {
+              "id": "verb",
+              "text": "Verb"
+            },
+            "text": "example"
+          },
+          {
+            "inflectionOf": [
+              {
+                "id": "example",
+                "text": "example"
+              }
+            ],
+            "language": "en",
+            "lexicalCategory": {
+              "id": "noun",
+              "text": "Noun"
+            },
+            "text": "example"
+          }
+        ],
+        "word": "example"
+      }
+    ]
 
   def index
-    @data = 'data'
     respond_to do |format|
-      format.html { render 'index' }
-      format.json { render json: @data, status: 200 }
+      # format.html { render 'index' }
+      format.json { render json: @results, status: 200 }
     end
   end
 end
