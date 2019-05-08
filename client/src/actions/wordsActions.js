60ffe4799706632e62856246963b30aec47e80a3
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-08 11:49:41 -0400

    Push all changes from previous commit

--- client/src/actions/wordsActions.js
+++ client/src/actions/wordsActions.js
@@ -1,10 +1,19 @@
 const LocalBaseUrl = 'http://localhost:3001/api/v1';
+
 const OxfordBaseUrl = 'https://od-api.oxforddictionaries.com/api/v2';
+let data = {
+  method: 'GET',
+  headers: {
+    "Accept": "application/json",
+    "app_id": process.env.REACT_APP_OXFORD_API_ID,
+    "app_key": process.env.REACT_APP_OXFORD_API_KEY
+  }
+};
 
 export const fetchWord = word => {
-  // each word has a text attr which gives the word itself
+  // words should be lowercase and stripped of whitespace
   return dispatch => {
-    fetch(`{OxfordBaseUrl}/entries/en-us/{word.text}`)
+    fetch(`{OxfordBaseUrl}/thesaurus/en/{word}`, data)
       .then(response => response.json())
       .then(data => dispatch({
         type: 'FETCH_WORD',
