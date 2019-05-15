1fa37c5deac79fe0892dc739a9cc4fe3c864579d
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-15 12:12:22 -0400

    Switch fetch call from Oxford API to Merriam-Webster

--- client/src/actions/wordsActions.js
+++ client/src/actions/wordsActions.js
@@ -1,27 +1,19 @@
 const LocalBaseUrl = 'http://localhost:3001/api/v1';
 
-const OxfordBaseUrl = 'https://od-api.oxforddictionaries.com/api/v2';
+const MerriamWebsterBaseUrl = 'https://dictionaryapi.com/api/v3/references/collegiate/json/';
 
 export const fetchWord = word => {
-  // words should be lowercase and stripped of whitespace
-  let data = {
-    method: 'GET',
-    headers: {
-      "Accept": "application/json",
-      "app_id": process.env.REACT_APP_OXFORD_API_ID,
-      "app_key": process.env.REACT_APP_OXFORD_API_KEY
-    }
-  };
+  let url = `${MerriamWebsterBaseUrl}${word.text}?key=13fc9042-8226-4650-b526-bbbb2386848e`;
   return dispatch => {
-    fetch(`${OxfordBaseUrl}/thesaurus/en/${word.text}`, data)
-      .then(response => response.json())
-      .then(response => dispatch({
-        type: 'FETCH_WORD_DATA',
-        payload: {
-          id: word.id,
-          response: response
-        }
-      }))
-      .catch(err => console.log(err));
+    fetch(url, { method: 'GET' })
+    .then(response => response.json())
+    .then(response => dispatch({
+      type: 'FETCH_WORD_DATA',
+      payload: {
+        id: word.id,
+        response: response
+      }
+    }))
+    .catch(err => console.log(err));
   };
 };
