24b47b3683b276750ae9f2e843086f0037f3488c
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-21 14:06:13 -0400

    Create new action to fetch data from local API. Refactored actions according to variable name changes

--- client/src/reducers/wordsReducer.js
+++ client/src/reducers/wordsReducer.js
@@ -5,25 +5,27 @@ export default function wordsReducer(state = {
   words: [],
   loading: false
 }, action) {
-  let word, words, selectedWord;
+  let word, words, selectedWord, response;
   switch (action.type) {
 
     case 'ADD_WORDS':
-      words = action.words.toLowerCase().concat(' ').split(' ').filter(word => word.length > 2);
-      words = words.map(text => {
+      words = action.words.split(' ');
+      words = words.map((text, index) => {
         return {
           id: cuidFn(),
+          index: index,
           text: text,
           originalText: text,
           isFetched: false,
-          examples: []
+          message: '',
+          options: []
         };
       })
       console.log('new words: ' + words.map(w=>' '+w.text+' ') );
 
       return { ...state, words: [...words] };
 
-    case 'BEGIN_FETCH_WORD_DATA':
+    case 'BEGIN_FETCH_WORD':
       console.log('beginning fetch, setting loading to true');
       console.log(state)
       return { ...state, loading: true };
@@ -32,24 +34,20 @@ export default function wordsReducer(state = {
       // find the right word in the words array based on its id and replace it with the new text
       selectedWord = state.words.filter(word => word.id === action.payload.id)[0];
       selectedWord.text = action.payload.newText;
-      // eliminate selectedWord from the current state
-      words = state.words.filter(word => word.id !== selectedWord.id)
-
       console.log(`replaced word from ${selectedWord.originalText} to  ${selectedWord.text}`)
       // add selectedWord back into state
-      return { ...state, words: [...words, selectedWord] };
+      return { ...state, words: [...state.words] };
 
-    case 'FETCH_WORD_DATA':
-      let response = action.payload.response;
+    case 'UPDATE_EXTERNAL_WORD':
+      response = action.payload.response;
       // find relevant word obect in texts array
       selectedWord = state.words.filter(word => word.id === action.payload.id)[0];
-      words = state.words.filter(word => word.id !== selectedWord.id);
       // filter out the response entry choices that don't match the original text
       response = response.filter(res => res.hwi.hw === selectedWord.text);
       // push each matching entry into the word object's examples array
-      response.map(entry => selectedWord.examples.push(entry));
+      response.map(entry => selectedWord.options.push(entry));
       // map over each example, format them into an object with extracted data stored as keys, and store it into the current word object's examples array
-      selectedWord.examples = selectedWord.examples.map(entry => {
+      selectedWord.options = selectedWord.options.map(entry => {
         return {
           id: cuidFn(),
           usage: entry.fl,
@@ -57,22 +55,46 @@ export default function wordsReducer(state = {
           synonyms: entry.def[0].sseq[0][0][1].syn_list[0].map(syn => syn.wd)
         };
       });
-      if (selectedWord.examples.length > 0) {
+      if (selectedWord.options.length > 0) {
         selectedWord.isFetched = true;
       }
-      console.log(`new word has ${selectedWord.examples.length} examples`);
+      console.log(`new external word has ${selectedWord.options.length} examples`);
       console.log('its isFetched property should be true: ' + selectedWord.isFetched);
-      return { ...state, words: [...words, selectedWord] };
+      return { ...state, words: [...state.words] };
 
-    case 'END_FETCH_WORD_DATA':
+    case 'UPDATE_API_WORD':
+      response = action.payload.response;
+      selectedWord = state.words.filter(word => word.id === action.payload.id)[0];
+      // add info to selectedWord:
+      selectedWord.options = response.data.attributes.options.map(option => {
+        return {
+          id: cuidFn(),
+          usage: option.usage,
+          sense: option.sense,
+          synonyms: option.synonyms.split(',')
+        };
+      });
+      if (selectedWord.options.length > 0) {
+        selectedWord.isFetched = true;
+      }
+      console.log(`new api word has ${selectedWord.options.length} examples`);
+      console.log('its isFetched property should be true: ' + selectedWord.isFetched);
+      return { ...state, words: [...state.words] };
+
+    case 'END_FETCH_WORD':
       console.log('ending fetch, setting loading to false');
-      return { words: state.words, loading: false };
+      return { ...state, words: [...state.words], loading: false };
 
     // case 'EXPAND_WORD':
     //   word = state.words.find(word => word.id === action.payload.id);
     //   word.expanded = true;
     //   console.log('word should be expanded:  ' + word.expanded);
     //   return { ...state, words: state.words };
+
+    // case 'ABORT_FETCH_WORD':
+    //   selectedWord = state.words.filter(word => word.id === action.payload.id)[0];
+    //   selectedWord.message = 'This word is too short to look up.'
+    //   return { ...state, words: [...state.words] }
 
     default:
       return state;
