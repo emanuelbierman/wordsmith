47525f2a2471e226493a7042329c5882cbb834a0
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-27 15:42:03 -0400

    Refactor API requests into two functions, fetchWord and postWord

--- client/src/actions/wordsActions.js
+++ client/src/actions/wordsActions.js
@@ -2,7 +2,7 @@ const LocalBaseUrl = 'http://localhost:3001/api/v1/words';
 
 const MerriamWebsterBaseUrl = 'https://dictionaryapi.com/api/v3/references/thesaurus/json/';
 
-export const shouldFetchWord = word => {
+export const fetchWord = word => {
   let api_url = `${LocalBaseUrl}/${word.text}`;
   let merriam_url = `${MerriamWebsterBaseUrl}${word.text}?key=6ee4458e-aa4b-4df1-b3fb-ee242052a997`;
 
@@ -36,14 +36,6 @@ export const shouldFetchWord = word => {
               }
             );
           dispatch({type: 'END_FETCH_WORD'});
-          // post word data to rails API
-          // set up a conditional here: only post if response object is valid
-          fetch(api_url, { method: 'POST', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, body: response })
-          .then(response => response.json())
-          .then(response => {
-            // is this action even necessary? what does a successful/unsuccessful POST request look like?
-            dispatch({ type: 'POSTED_WORD', payload: {id: word.id, response: response}})
-          })
           }
         )
       }
@@ -51,24 +43,15 @@ export const shouldFetchWord = word => {
   }
 }
 
-export const fetchWord = word => {
-  let merriam_url = `${MerriamWebsterBaseUrl}${word.text}?key=6ee4458e-aa4b-4df1-b3fb-ee242052a997`;
+export const postWord = word => {
+  let api_url = `${LocalBaseUrl}/${word.text}`;
+  // post word data to rails API
   return dispatch => {
-    fetch(merriam_url, { method: 'GET' })
+    fetch(api_url, { method: 'POST', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, body: word })
     .then(response => response.json())
     .then(response => {
-      dispatch({
-          type: 'UPDATE_EXTERNAL_WORD',
-          payload: {
-            id: word.id,
-            response: response
-            }
-          }
-        );
-      dispatch({type: 'END_FETCH_WORD'});
-      // post word data in rails API
-      }
-    )
-    .catch(err => console.log(err));
-  };
-};
+      // is this action even necessary? what should a successful/unsuccessful POST request return?
+      dispatch({ type: 'POSTED_WORD', payload: {id: word.id, response: response}})
+    })
+  }
+}
