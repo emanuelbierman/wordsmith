58dd7b590631d519e68a013aa0ca8bc7bde167da
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-30 14:08:04 -0400

    Specify endpoints and let the controller know to expect dynamic parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :words
+      resources :words, only: [:index]
+      get '/words/:text', to: 'words#show'
+      post '/words/:text', to: 'words#create'
     end
   end
 end
