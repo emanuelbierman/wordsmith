20d33989a3ba2a65310dbee67a423871dbb73fa4
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-30 14:36:40 -0400

    Write posted_word and failed_word actions according to the response status. Remove conditional logic for word.isFetched because already present in component. Refactor word updating logic handling to match the appropriate API response. Remove trailing whitespace if present in user text input.

--- client/src/reducers/wordsReducer.js
+++ client/src/reducers/wordsReducer.js
@@ -4,12 +4,14 @@ export const cuidFn = cuid;
 export default function wordsReducer(state = {
   words: [],
   loading: false
+  // create empty loading array, and inside ADD_WORDS action, grab the index of the word and create an object inside the loading array, with a key of index and a value of false. Then inside BEGIN and END actions, find the object's index and set the value of the corresponding key to true
 }, action) {
   let word, words, selectedWord, response;
   switch (action.type) {
 
     case 'ADD_WORDS':
-      words = action.words.split(' ');
+      // split into array and eliminate trailing white space if present
+      words = action.words.split(' ').filter(w => w.length > 0);
       words = words.map((text, index) => {
         return {
           id: cuidFn(),
@@ -17,12 +19,11 @@ export default function wordsReducer(state = {
           text: text,
           originalText: text,
           isFetched: false,
-          message: '',
+          posted: false,
           options: []
         };
       })
       console.log('new words: ' + words.map(w=>' '+w.text+' ') );
-
       return { ...state, words: [...words] };
 
     case 'BEGIN_FETCH_WORD':
@@ -55,35 +56,48 @@ export default function wordsReducer(state = {
           synonyms: entry.def[0].sseq[0][0][1].syn_list[0].map(syn => syn.wd)
         };
       });
-      if (selectedWord.options.length > 0) {
-        selectedWord.isFetched = true;
-      }
-      console.log(`new external word has ${selectedWord.options.length} examples`);
-      console.log('its isFetched property should be true: ' + selectedWord.isFetched);
+      selectedWord.isFetched = true;
+      console.log(`new external word has ${selectedWord.options.length} options`);
       return { ...state, words: [...state.words] };
 
     case 'UPDATE_API_WORD':
       response = action.payload.response;
       selectedWord = state.words.filter(word => word.id === action.payload.id)[0];
       // add info to selectedWord:
-      selectedWord.options = response.data.attributes.options.map(option => {
+      selectedWord.options = response.options.map(option => {
         return {
           id: cuidFn(),
           usage: option.usage,
           sense: option.sense,
-          synonyms: option.synonyms.split(',')
+          synonyms: JSON.parse(option.synonyms)
         };
       });
-      if (selectedWord.options.length > 0) {
-        selectedWord.isFetched = true;
-      }
-      console.log(`new api word has ${selectedWord.options.length} examples`);
-      console.log('its isFetched property should be true: ' + selectedWord.isFetched);
+      selectedWord.isFetched = true;
+      console.log(`new api word has ${selectedWord.options.length} options`);
       return { ...state, words: [...state.words] };
 
     case 'END_FETCH_WORD':
       console.log('ending fetch, setting loading to false');
+      console.log(state)
       return { ...state, words: [...state.words], loading: false };
+
+    case 'POSTED_WORD':
+      selectedWord = state.words.filter(word => word.id === action.payload.id)[0];
+      // does the selectedWord match the newly created word?
+      if (selectedWord.text === action.payload.response.text && selectedWord.options.length === action.payload.response.options.length) {
+        selectedWord.posted = true;
+        console.log(`${selectedWord.text} has been successfully saved to backend API`)
+      }
+      return { ...state, words: [...state.words] };
+
+    case 'POST_FAILED':
+      selectedWord = state.words.filter(word => word.id === action.payload.id)[0];
+      // does the selectedWord match the newly created word?
+      if (selectedWord.text === action.payload.response.text && selectedWord.options.length === action.payload.response.options.length) {
+        selectedWord.posted = true;
+        console.log(`${selectedWord.text} already exists`)
+      }
+      return { ...state, words: [...state.words] };
 
     // case 'EXPAND_WORD':
     //   word = state.words.find(word => word.id === action.payload.id);
