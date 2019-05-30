6446a7ae3ce8f11658eff970302b5ba1fcfe84e9
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-30 14:09:26 -0400

    Refactor #show and #create rooutes to accept and return json via params

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -6,16 +6,32 @@ class Api::V1::WordsController < ApplicationController
   end
 
   def show
-    word = Word.all.find_by(text: params[:id])
-    render json: word
+    if Word.all.find_by(text: params[:text])
+      word = Word.all.find_by(text: params[:text])
+      render json: word.to_json(include: :options), status: 200
+    else
+      render json: {}, status: 404
+    end
   end
 
   def create
-    binding.pry
-    word = Word.all.find_by(text: params[:id])
-    unless word
+    word = Word.find_or_create_by(text: params[:text])
+    if word.options.length === 0
       # access the payload through params
-      # create Word and Option objects
+      # create Option objects
+      params[:word][:options].each do |option|
+        Option.create! word_id: word.id, usage: option[:usage], sense: option[:sense], synonyms: option[:synonyms]
+      end
+      if word.options.length > 0
+        # the new Word and Options were created
+        render json: word.to_json(include: :options), status: 201
+      else
+        # the Options creation failed
+        render json: {}, status: 404
+      end
+    else
+      # the word already exists
+      render json: word.to_json(include: :options), status: 200
     end
   end
 
