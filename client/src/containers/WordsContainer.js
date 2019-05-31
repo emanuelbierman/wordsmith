c853cd85b997efeb1600c8fb05355b5857a93366
emanuelbierman <emanuel.bierman@gmail.com>
2019-05-31 11:22:55 -0400

    Refactor WordsContainer to conditionally render an empty card until the user adds words

--- client/src/containers/WordsContainer.js
+++ client/src/containers/WordsContainer.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import WordContainer from './WordContainer';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Card, Badge } from 'reactstrap';
 
 class WordsContainer extends Component {
 
   render() {
+    let wordsContainer;
+    if (this.props.words.length > 0) {
+      wordsContainer = this.props.words.map((word, index) => <Col key={index} xs="auto"><WordContainer word={word} key={word.id} replaceWord={this.props.replaceWord} expandWord={this.props.expandWord} loading={this.props.loading}/></Col>)
+    } else {
+      wordsContainer = <>
+        <Card body outline color="info">
+          <br></br>
+          <br></br>
+          <h4>Welcome to the forge. Type in the text bar to fill this space with words!</h4>
+          <br></br>
+          <br></br>
+        </Card>
+      </>;
+    }
     return(
       <Container className="text-info">
         <br></br>
-        <p>choose a word to expand it => choose the usage:sense pair that fits your meaning => begin forging!</p>
+        <Row>
+          <h5><Badge color="info">1</Badge> forge your words => <Badge color="info">2</Badge> expand and collapse to explore =></h5>
+        </Row>
+        <Row>
+          <h5><Badge color="info">3</Badge> choose the usage:sense pair that fits your meaning => <Badge color="info">4</Badge> click on synonyms to swap and experiment!</h5>
+        </Row>
         <br></br>
         <Row xs="auto">
-          {this.props.words.map((word, index) => <Col key={index} xs="auto"><WordContainer word={word} key={word.id} replaceWord={this.props.replaceWord} expandWord={this.props.expandWord} loading={this.props.loading}/></Col>)}
+          {wordsContainer}
         </Row>
       </Container>
     )
