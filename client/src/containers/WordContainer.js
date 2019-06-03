3c52a5604ca8d73b7505f7110634ca96babb7f8e
emanuelbierman <emanuel.bierman@gmail.com>
2019-06-03 10:58:06 -0400

    Add local state to toggle expansion of Word card. Add condition to only post a word to backend API if it has not already been posted. Add conditional render for external API responses that are not formatted properly.

--- client/src/containers/WordContainer.js
+++ client/src/containers/WordContainer.js
@@ -9,11 +9,18 @@ class WordContainer extends Component {
   // const word = this.props.word;
 
   state = {
-    text: this.props.word.text
+    text: this.props.word.text,
+    expandOptions: false
   }
 
-  beginFetch = () => {
-    this.props.fetchWord(this.props.word);
+  toggleExpandAndFetch = () => {
+    this.setState(state => ({
+      expandOptions: !state.expandOptions
+    }));
+    // only fetch if it hasn't been fetched yet
+    if (!this.props.word.isFetched) {
+      this.props.fetchWord(this.props.word);
+    }
   }
 
   swapText = synonym => {
@@ -21,31 +28,30 @@ class WordContainer extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // if word has loaded, push to backend API
-    if (this.props.word.isFetched) {
+    if (this.props.word.isFetched && !this.props.word.posted) {
+      // if word has loaded and hasn't already been posted, post to backend API
       this.props.postWord(this.props.word);
     }
   }
 
   render() {
-    if (this.props.loading) {
-      return <div>Loading...</div>
-    }
     let options;
-    if (!this.props.loading && this.props.word.isFetched && this.props.word.options.length > 0) {
+    if (!this.state.expandOptions) {
+      options = null;
+    } else if (this.state.expandOptions && this.props.loading) {
+      options = <div>Loading...</div>;
+    } else if (this.state.expandOptions && !this.props.word.isFetched) {
+      options = <div>There has been an error loading the information you requested. Try clicking again, try another word, or refresh the page...</div>;
+    } else if (this.state.expandOptions && this.props.word.isFetched) {
       options = this.props.word.options.map(option => {
         return <WordOption key={option.id} option={option} isFetched={this.props.word.isFetched}
         fetchWord={this.props.fetchWord} replaceWord={this.props.replaceWord}
         swapText={this.swapText} expandWord={this.props.expandWord} wordId={this.props.word.id}/>
       });
-    } else if (this.props.word.isFetched && this.props.word.options.length < 0) {
-      options = 'There has been an error loading the information you requested. Try refreshing again or try another word!';
-    } else {
-      options = null;
     }
     return(
       <Card body outline color="info">
-        <h3 onClick={this.beginFetch}>{this.state.text}</h3>
+        <h3 onClick={this.toggleExpandAndFetch}>{this.state.text}</h3>
         {options}
       </Card>
     )
