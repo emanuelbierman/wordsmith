2cb1a135b27d3ba231b2f24488322a09f43affd2
emanuelbierman <emanuel.bierman@gmail.com>
2019-06-04 10:38:35 -0400

    Add two new routes. Extract header and navbar into Title component, to be unconditionally rendered

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import WordsContainer from './containers/WordsContainer';
-// import PinnedSentencesContainer from './containers/PinnedSentencesContainer';
+import Title from './components/Title';
 import Home from './components/Home';
+import ExampleContainer from './containers/ExampleContainer';
+import Tools from './components/Tools';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" component={Home} />
+        <Title/>
+        <Route exact path="/" component={Home} />
+        <Route path="/example" component={ExampleContainer} />
+        <Route path="/tools" component={Tools} />
       </Router>
     );
   }
--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col, Nav, NavItem, NavLink } from 'reactstrap';
 
 class Title extends Component {
   render() {
     return(
       <Container className="text-info">
-        <h1>Wordsmith</h1>
+        <Row>
+          <Col xs="6">
+            <h1>Wordsmith</h1>
+          </Col>
+          <Col xs="3">
+            <Nav>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/example">Example</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/tools">Tools</NavLink>
+              </NavItem>
+            </Nav>
+          </Col>
+        </Row>
       </Container>
     )
   }
